ef63b37240b2db07c2a8fa73417df84f112672a7
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 16:02:46 +0800

    1

--- src/Main04/Main04.java
+++ src/Main04/Main04.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Main04 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m =n/2;
+        boolean flag=true;
+        for(int i= 2; i<=m ;i++)
+        {
+            if(n%i ==0)
+            {
+                flag =false;
+               break;
+            }
 
-
-
+        }
+        if(flag) {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
     }
 }
